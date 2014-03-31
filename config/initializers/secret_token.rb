27841325e9a8563a5384ed8c2bb500352b695daa
Abhijeet K <user@email.com>
2014-03-31 16:04:42 +0530

    Start Static Proj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MostlyStatic::Application.config.secret_key_base = '804d9d07de40fcac4c6e41afbfcecc1b48646404be924d647ad5ef0f5fefae26abc6d969a7f425ecf1492d53eeb5594f4213a1f0ceeb0a09c6528be10cd432c3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MostlyStatic::Application.config.session_store :cookie_store, key: '_mostly_static_session'
